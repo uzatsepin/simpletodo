a641da194c7d0ed9650664a98fab08eea3dc1c68
Andrii Zatsepin <uzatsepin@gmail.com>
2022-05-12 16:10:32 +0300

    feat: added btn remove todo, added logic of removing

--- src/components/screens/Home/item/TodoItem.jsx
+++ src/components/screens/Home/item/TodoItem.jsx
@@ -1,17 +1,28 @@
 import React from 'react'
 import Check from './Check'
 import cn from 'classnames';
+import { BsTrash } from 'react-icons/bs';
 
-function TodoItem({todo, changeTodo}) {
+function TodoItem({todo, changeTodo, removeTodo}) {
   return (
-    <button className='flex items-center mb-4 rounded-3xl bg-gray-800 p-5 w-full' onClick={() => changeTodo(todo.id)}>
-      <Check isCompleted={todo.isCompleted}/>
-      <span className={cn({
-        'line-through' : todo.isCompleted,
-        })}>
-          {todo.title}
-        </span>
-    </button>
+    <div className='flex items-center justify-between mb-4 rounded-3xl bg-gray-800 p-5 w-full' 
+            onClick={() => changeTodo(todo.id)}>
+      
+      <button className='flex items-center'>
+
+        <Check isCompleted={todo.isCompleted}/>
+
+        <span className={cn({
+          'line-through' : todo.isCompleted,
+          })}>
+            {todo.title}
+          </span>
+        </button>
+        <button onClick={() => removeTodo}>
+           <BsTrash size={22} className='text-gray-600 hover:text-red-700 transition-colors ease-in-out duration-300' />
+        </button>
+        
+    </div>
   )
 }
 
